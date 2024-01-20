8dd41d28be2fcf2d958dd1db8d6c712718970a25
Cyprian Zdebski <cyprianz5mail@gmail.com>
2024-01-20 23:46:46 +0100

    add eslint, husky, prettier support

---
+++ lint-staged.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    // this will check Typescript files
+    "**/*.(ts|tsx)": () => "pnpm exec tsc --noEmit",
+
+    // This will lint and format TypeScript and                                             //JavaScript files
+    "**/*.(ts|tsx|js)": filenames => [
+        `pnpm exec eslint --fix ${filenames.join(" ")}`,
+        `pnpm exec prettier --write ${filenames.join(" ")}`,
+    ],
+
+    // this will Format MarkDown and JSON
+    "**/*.(md|json)": filenames =>
+        `pnpm exec prettier --write ${filenames.join(" ")}`,
+};
